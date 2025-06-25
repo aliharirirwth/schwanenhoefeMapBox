9baa905e4798eb6e7f1766a6398c9537023a5bff
aliharirirwth <ali.hariri@rwth-aachen.de>
2025-06-26 01:19:08 +0200

    feat: update companies.js with definitive company/building/entrance mapping based on map and excel data

--- client/public/companies.js
+++ client/public/companies.js
@@ -1,88 +1,48 @@
-// Hardcoded company and building data for Schwanenhöfe
+// Ultimate company and building data for Schwanenhöfe
 export const companies = [
-  {
-    name: "WHU",
-    logo: "image_logos/whu.jpg",
-    building: "224A",
-    longitude: 6.8147,
-    latitude: 51.2193,
-    showTriangle: true
-  },
-  {
-    name: "TRG",
-    logo: "image_logos/trg.png",
-    building: "220A",
-    longitude: 6.814177,
-    latitude: 51.219274,
-    showTriangle: true
-  },
-  {
-    name: "Zeron",
-    logo: "image_logos/zeron.png",
-    building: "220A",
-    longitude: 6.814177,
-    latitude: 51.219274
-  },
-  {
-    name: "Alfons",
-    logo: "image_logos/alfons.jpg",
-    building: "224A",
-    longitude: 6.8147,
-    latitude: 51.2193
-  },
-  {
-    name: "Camp-1",
-    logo: "image_logos/camp-1.jpg",
-    building: "230A",
-    longitude: 6.814177,
-    latitude: 51.219274
-  },
-  {
-    name: "Sopexa",
-    logo: "image_logos/sopexa.png",
-    building: "224A",
-    longitude: 6.8147,
-    latitude: 51.2193
-  },
-  {
-    name: "Ifenius",
-    logo: "image_logos/ifenius.png",
-    building: "228A",
-    longitude: 6.814177,
-    latitude: 51.219274
-  },
-  {
-    name: "Relyens",
-    logo: "image_logos/relyens.png",
-    building: "224A",
-    longitude: 6.8147,
-    latitude: 51.2193
-  },
-  {
-    name: "SV Group",
-    logo: "image_logos/svgroup.png",
-    building: "220A",
-    longitude: 6.814177,
-    latitude: 51.219274
-  },
-  {
-    name: "HM Partner",
-    logo: "image_logos/hmpartner.jpg",
-    building: "224A",
-    longitude: 6.8147,
-    latitude: 51.2193
-  },
-  // Add more real companies here as needed
+  { name: "alessandro International GmbH", address: "Erkrather Straße 228 d", building_code: "228D", entrance_code: "228D" },
+  { name: "alessandro International GmbH Shop", address: "Erkrather Straße 224 d", building_code: "224D", entrance_code: "224D" },
+  { name: "Alfons & Alfreda Managment GmbH", address: "Erkrather Straße 230", building_code: "230", entrance_code: "230" },
+  { name: "AWO Familienglobus gGmbh", address: "Erkrather Straße 222", building_code: "222", entrance_code: "222" },
+  { name: "babiel GmbH", address: "Erkrather Straße 224 a", building_code: "224A", entrance_code: "224A" },
+  { name: "Camp Physiotherapie", address: "Erkrather Straße 224 b", building_code: "224B", entrance_code: "224B" },
+  { name: "Cave Portuguesa GbR", address: "Erkrather Straße 218b", building_code: "218B", entrance_code: "218B" },
+  { name: "Dag Seemann", address: "Erkrather Straße 216a", building_code: "216A", entrance_code: "216A" },
+  { name: "Dr. Thompson's Gastro GmbH & Co. KG", address: "Erkrather Straße 232", building_code: "232", entrance_code: "232" },
+  { name: "Event Theater Schwanenhöfe", address: "Erkrather Straße 234 b", building_code: "234B", entrance_code: "234B" },
+  { name: "FitX Deutschland GmbH", address: "Erkrather Straße 230", building_code: "230", entrance_code: "230" },
+  { name: "FMR Rechtsanwälte", address: "Erkrather Straße 220 b", building_code: "220B", entrance_code: "220B" },
+  { name: "Frango Portugûes GbR", address: "Erkrather Straße 224", building_code: "224", entrance_code: "224" },
+  { name: "G-Star GmbH", address: "Erkrather Straße 234 d", building_code: "234D", entrance_code: "234D" },
+  { name: "GFG Life GmbH", address: "Erkrather Straße 224 a", building_code: "224A", entrance_code: "224A" },
+  { name: "H.J. Heinz GmbH", address: "Erkrather Straße 228 b", building_code: "228B", entrance_code: "228B" },
+  { name: "Hartmann, Mathias und Partner", address: "Erkrather Straße 234 a", building_code: "234A", entrance_code: "234A" },
+  { name: "Hase und Igel Food + Beverage GmbH", address: "Erkrather Straße 226", building_code: "226", entrance_code: "226" },
+  { name: "HERE AND NOW GmbH & Co. KG", address: "Erkrather Straße 234 c", building_code: "234C", entrance_code: "234C" },
+  { name: "Iberian Living home", address: "Erkrather Straße 228 c", building_code: "228C", entrance_code: "228C" },
+  { name: "Implementum Real Estate GmbH", address: "Erkrather Straße 230", building_code: "230", entrance_code: "230" },
+  { name: "IST Studieninstitut GmbH", address: "Erkrather Straße 220 a-c", building_code: "220A,220B,220C", entrance_code: "220A" },
+  { name: "JP KOM GmbH", address: "Erkrather Straße 234 b", building_code: "234B", entrance_code: "234B" },
+  { name: "JustSpices GmbH", address: "Erkrather Straße 228 a", building_code: "228A", entrance_code: "228A" },
+  { name: "NanoGiants GmbH", address: "Erkrather Straße 230", building_code: "230", entrance_code: "230" },
+  { name: "OnlineDialog GmbH", address: "Erkrather Straße 216a", building_code: "216A", entrance_code: "216A" },
+  { name: "Parkett Strehl GmbH", address: "Erkrather Straße 228 a", building_code: "228A", entrance_code: "228A" },
+  { name: "Pasabahce Glas GmbH", address: "Erkrather Straße 228 b", building_code: "228B", entrance_code: "228B" },
+  { name: "Phase 5 GmbH", address: "Erkrather Straße 230", building_code: "230", entrance_code: "230" },
+  { name: "Piratas Werbeagentur GmbH & Co. KG", address: "Erkrather Straße 228 a", building_code: "228A", entrance_code: "228A" },
+  { name: "Relyens Mutual Insurance", address: "Erkrather Straße 228 b", building_code: "228B", entrance_code: "228B" },
+  { name: "Reviderm AG", address: "Erkrather Straße 228 b", building_code: "228B", entrance_code: "228B" },
+  { name: "Schmeing Bau GmbH", address: "Erkrather Straße 224 d", building_code: "224D", entrance_code: "224D" },
+  { name: "SOFACOMPANY GmbH", address: "Erkrather Straße 228 a", building_code: "228A", entrance_code: "228A" },
+  { name: "SV Group GmbH", address: "Erkrather Straße 230", building_code: "230", entrance_code: "230" },
+  { name: "The Reach Group GmbH", address: "Erkrather Straße 228 a", building_code: "228A", entrance_code: "228A" },
+  { name: "WHU Wissenschaftliche Hochschule für Unternehmensführung", address: "Erkrather Straße 224 a, 218, 228 a-c, 230", building_code: "224A,218,228A,228B,228C,230", entrance_code: "224A" },
+  { name: "Zeron GmbH", address: "Erkrather Straße 234 a", building_code: "234A", entrance_code: "234A" },
+  { name: "4stairs", address: "Erkrather Straße 230", building_code: "230", entrance_code: "230" },
+  { name: "ifenius", address: "Erkrather Straße 228 a", building_code: "228A", entrance_code: "228A" }
 ];
 
-// Get unique building numbers and ensure each has a triangle
-const uniqueBuildings = new Set(companies.map(c => c.building));
-companies.forEach(company => {
-  // If this is the first company in its building, make it show the triangle
-  if (!companies.some(c => c.building === company.building && c.showTriangle)) {
-    company.showTriangle = true;
-  }
-});
-
-// Export building numbers as an array of actual building numbers
-export const buildingNumbers = Array.from(uniqueBuildings);
+// Unique building numbers for grid/list
+export const buildingNumbers = [
+  "216A", "216B", "218A", "218B", "220A", "220B", "222", "224", "224A", "224B", "224C", "224D", "226", "228A", "228B", "228C", "228D", "230", "232", "234A", "234B", "234C", "234D"
+];
