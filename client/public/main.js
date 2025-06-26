e096a8f0567ee821325affea20c19ab9c28c64cd
aliharirirwth <ali.hariri@rwth-aachen.de>
2025-06-26 03:28:36 +0200

    Remove entrance verification markers; only show anchor on user selection

--- client/public/main.js
+++ client/public/main.js
@@ -192,7 +192,6 @@ function initMapFeatures() {
 
         // Add triangle icons and building labels
         addBuildingMarkers(labelLayerId);
-        addEntranceVerificationMarkers();
     });
 }
 
@@ -267,18 +266,6 @@ function addBuildingMarkers(labelLayerId) {
                     'text-halo-width': 1
                 }
             }, labelLayerId);
-        });
-    });
-}
-
-function addEntranceVerificationMarkers() {
-    import('./companies.js').then(module => {
-        const { entrances } = module;
-        entrances.forEach(e => {
-            new mapboxgl.Marker({ color: 'blue' })
-                .setLngLat([e.longitude, e.latitude])
-                .setPopup(new mapboxgl.Popup().setText(`${e.entrance_code} (${e.building_label})`))
-                .addTo(map);
         });
     });
 }
